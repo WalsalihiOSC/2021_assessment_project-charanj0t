9fa662494a091e28ddefc384720c9454a1759b3b
charanj0t <79234197+charanj0t@users.noreply.github.com>
2021-08-06 10:49:26 +1200

    Add files via upload

--- gamemath.py
+++ gamemath.py
@@ -9,6 +9,7 @@ class Window1:
    list_entry_add, list_entry_sub, list_entry_div, list_entry_mul = [],[],[],[]
    addAns, subAns, divAns, mulAns  = [],[],[],[]
    score = 0
+   
    #default constructor, calls when an object is created
    def __init__(self, root, Name):
       self.window = root
@@ -19,3 +20,21 @@ class Window1:
       self.year_level = tk.StringVar()
       self.TYPE_ = tk.StringVar()
       self.score = tk.StringVar()
+
+      #create a mainframe widget
+      self.mainFrame = Frame(self.window,bg="#4A86E8")
+      self.mainFrame.pack(fill="both")
+
+      #creating all frame widgets that are needed
+      self.frame0 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame1 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame2 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame3 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame4 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame5 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame6 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame7 = Frame(self.mainFrame,bg="#4A86E8")
+      self.frame8 = Frame(self.mainFrame,bg="#4A86E8")
+
+      #showing the frame0, when we pack any frame, it shows on screen
+      self.frame0.pack(fill="x")
